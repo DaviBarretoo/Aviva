4631dda1d11ad7ad41ca9aea1be5044d6403e087
Davi Barreto <davibarretojkda@gmail.com>
2024-04-15 10:43:58 -0300

    Finalized Routes

---
+++ src/Pages/Proverbios/index.jsx
@@ -0,0 +1,177 @@
+import React from 'react';
+import { Container } from './styled';
+
+export default function Salmos() {
+  return (
+    <Container>
+      <h2>Provérbios</h2>
+      <p>
+        <p>
+          <h1>1</h1> Ao ser humano foi concedida a capacidade de planejar,
+          entretanto, é o SENHOR quem dá a palavra certa.
+        </p>
+
+        <p>
+          <h1>2</h1> Todos os caminhos do homem parecem certos aos seus olhos,
+          mas o SENHOR julga as verdadeiras motivações do coração.
+        </p>
+
+        <p>
+          <h1>3</h1> Consagra ao SENHOR todas as tuas obras e os teus planos
+          serão bem-sucedidos.
+        </p>
+
+        <p>
+          <h1>4</h1> O SENHOR criou tudo o que existe com um propósito definido;
+          até mesmo os ímpios para o dia do castigo.
+        </p>
+
+        <p>
+          <h1>5</h1> Abominável é ao SENHOR todo arrogante de coração; é
+          evidente que não ficará sem a devida punição.
+        </p>
+
+        <p>
+          <h1>6</h1> Por intermédio da misericórdia e da verdade se faz expiação
+          do pecado; e pelo temor do SENHOR, o ser humano evita todo o maligno.
+        </p>
+
+        <p>
+          <h1>7</h1> Quando as atitudes de uma pessoa são agradáveis ao SENHOR,
+          até os inimigos dessa pessoa vivem em paz com ela, pela vontade
+          divina.
+        </p>
+
+        <p>
+          <h1>8</h1> É muito melhor possuir poucos bens com honestidade do que
+          riquezas com injustiça.
+        </p>
+
+        <p>
+          <h1>9</h1> Em sua alma, o homem planeja seus caminhos, mas o SENHOR é
+          quem determina seus passos.
+        </p>
+
+        <p>
+          <h1>10</h1> Os lábios do rei falam com grande autoridade; contudo, sua
+          boca não deve jamais trair a justiça.
+        </p>
+
+        <p>
+          <h1>11</h1> Equilíbrio e pesos honestos vêm do SENHOR; por isso, Ele
+          deseja que a balança seja usada com justiça.
+        </p>
+
+        <p>
+          <h1>12</h1> A prática da impiedade é abominável para os governantes,
+          porque com equidade deve ser estabelecido o poder.
+        </p>
+
+        <p>
+          <h1>13</h1> Os lábios justos fazem a felicidade do rei, e ele ama as
+          pessoas que falam a verdade.
+        </p>
+
+        <p>
+          <h1>14</h1> A ira do rei é prenúncio de morte, mas o homem sábio
+          consegue acalmá-lo.
+        </p>
+
+        <p>
+          <h1>15</h1> A alegria no rosto do rei é sinal de vida; seu favor é
+          como generosa nuvem de chuva na primavera.
+        </p>
+
+        <p>
+          <h1>16</h1> É muito melhor conquistar a sabedoria do que o ouro puro.
+          É mais proveitoso obter o entendimento do que a prata mais valiosa.
+        </p>
+
+        <p>
+          <h1>17</h1> O caminho do justo evita o mal; quem guarda seu caminho
+          preserva sua vida.
+        </p>
+
+        <p>
+          <h1>18</h1> A soberba precede a ruína, o espírito arrogante vem antes
+          da queda.
+        </p>
+
+        <p>
+          <h1>19</h1> Melhor é ter espírito humilde entre os oprimidos do que
+          repartir um grande espólio com os arrogantes.
+        </p>
+
+        <p>
+          <h1>20</h1> Quem considera atentamente tudo o que fala, prospera, e
+          feliz é aquele que confia no SENHOR.
+        </p>
+
+        <p>
+          <h1>21</h1> O sábio de coração é considerado inteligente; quem fala
+          com equilíbrio tem o poder de convencer os outros.
+        </p>
+
+        <p>
+          <h1>22</h1> O entendimento é fonte de vida para aqueles que o possuem,
+          mas a insensatez traz castigos aos tolos.
+        </p>
+
+        <p>
+          <h1>23</h1> O coração do sábio ministra à sua boca, e seus lábios são
+          hábeis para o ensino.
+        </p>
+
+        <p>
+          <h1>24</h1> As palavras agradáveis são como um favo de mel, são doces
+          para a alma e revigoram a saúde e alegria de viver.
+        </p>
+
+        <p>
+          <h1>25</h1> Há caminhos que parecem certos ao ser humano, contudo, no
+          final conduzem à morte.
+        </p>
+
+        <p>
+          <h1>26</h1> A fome do trabalhador o obriga a trabalhar; é o seu
+          estômago que o impulsiona.
+        </p>
+
+        <p>
+          <h1>27</h1> O homem maligno está sempre à procura de praticar o mal;
+          até mesmo suas palavras são como o fogo devorador.
+        </p>
+
+        <p>
+          <h1>28</h1> O homem perverso vive provocando contendas, assim como o
+          difamador, que consegue separar os maiores amigos.
+        </p>
+
+        <p>
+          <h1>29</h1> O homem violento alicia seu próprio amigo e o guia pelas
+          trilhas do mal.
+        </p>
+
+        <p>
+          <h1>30</h1> Quem faz sinais maliciosos com os olhos planeja o mal;
+          quem franze os lábios já está a meio caminho da prática do que é ruim.
+        </p>
+
+        <p>
+          <h1>31</h1> O cabelo grisalho é uma coroa de experiência e esplendor,
+          que deve ser conquistada mediante uma vida justa.
+        </p>
+
+        <p>
+          <h1>32</h1> Muito melhor é o homem paciente que o guerreiro, mais vale
+          controlar as emoções e os ímpetos do que conquistar toda uma cidade!
+        </p>
+
+        <p>
+          <h1>33</h1> A sorte é lançada no colo, mas a decisão correta vem do
+          SENHOR!
+        </p>
+      </p>
+    </Container>
+  );
+}
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Mateus from '../Pages/Mateus';
-//import Proverbios from '../Pages/Proverbios';
-//import Salmos from '../Pages/Salmos';
+import Proverbios from '../Pages/Proverbios';
+import Salmos from '../Pages/Salmos';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/mateus" component={Mateus} />
+      <Route exact path="/Salmos" component={Salmos} />
+      <Route exact path="/proverbios" component={Proverbios} />
     </Switch>
   );
 }
