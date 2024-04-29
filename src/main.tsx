3a1c6f31fabf2fe147a5eb39ed25442387a50f93
Davi Barreto <davibarretojkda@gmail.com>
2024-04-28 22:31:06 -0300

    final update in routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header/header'
-import {Routes,Route, BrowserRouter} from 'react-router-dom'
+import {Routes,Route, HashRouter} from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import Mateus from './Pages/Mateus';
 import Principal from './Pages/Principal';
@@ -11,17 +11,17 @@ import Proverbios from './Pages/Proverbios';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
- <BrowserRouter>
+ <HashRouter>
     <Header/>
     <Globalstyle />
     <Routes>
     
-    <Route path='/' element={<Principal />}/>
+    <Route path='/Aviva' element={<Principal />}/>
     <Route path='/Aviva/Mateus' element={<Mateus />}/>
     <Route path='/Aviva/Salmos' element={<Salmos />}/>
     <Route path='/Aviva/Proverbios' element={<Proverbios />}/>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
     <ToastContainer
         position="top-left"
         autoClose={1000}
