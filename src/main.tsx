ae5ca064d150ddd5615b3a5fe2750cc71e50f942
Davi Barreto <davibarretojkda@gmail.com>
2024-04-28 22:27:43 -0300

    change in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header/header'
-import {Routes,Route, HashRouter} from 'react-router-dom'
+import {Routes,Route, BrowserRouter} from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import Mateus from './Pages/Mateus';
 import Principal from './Pages/Principal';
@@ -11,7 +11,7 @@ import Proverbios from './Pages/Proverbios';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
- <HashRouter>
+ <BrowserRouter>
     <Header/>
     <Globalstyle />
     <Routes>
@@ -21,7 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Route path='/Aviva/Salmos' element={<Salmos />}/>
     <Route path='/Aviva/Proverbios' element={<Proverbios />}/>
     </Routes>
-    </HashRouter>
+    </BrowserRouter>
     <ToastContainer
         position="top-left"
         autoClose={1000}
